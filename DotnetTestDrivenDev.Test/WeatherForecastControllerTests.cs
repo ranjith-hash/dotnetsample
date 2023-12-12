bcb25b11cf21db3511b4825b25fa0a347928b3fc
Jayaranjith Jayashankar <ranjith@Ranjiths-MacBook-Pro.local>
2023-12-12 15:12:43 +0530

    initial commit

---
+++ DotnetTestDrivenDev.Test/WeatherForecastControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using DotnetTestDrivenDev.Controllers;
+using Xunit;
+
+namespace MyWebApi.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsFiveWeatherForecasts()
+        {
+            // Arrange
+            var controller = new WeatherForecastController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.Equal(5, result.Count());
+        }
+
+        [Fact]
+        public void Get_ReturnsWeatherForecastsWithValidData()
+        {
+            // Arrange
+            var controller = new WeatherForecastController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            foreach (var item in result)
+            {
+                Assert.NotNull(item.Date);
+                Assert.InRange(item.TemperatureC, -20, 55);
+                Assert.Contains(item.Summary, WeatherForecastController.Summaries);
+            }
+        }
+    }
+}
---
+++ DotnetTestDrivenDev/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetTestDrivenDev.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var rng = new Random();
+        var startDate = DateTime.Now.Date;
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = rng.Next(-20, 55),
+            Summary = Summaries[rng.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    public static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+}
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string Summary { get; set; }
+}
+
